528403807570a2491108404be42cc47e5f8e5f81
MarianneLab <laballery.marianne@orange.fr>
2021-08-02 10:40:17 +0200

    install bootstrapp + création pages Home et 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "", component: HomePageComponent},
+{path: "not-found", component: FourOhFourComponent},
+{path: "**", redirectTo:"not-found"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
